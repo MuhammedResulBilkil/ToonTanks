87381be59335cdb0b0d5bff99afc006766ca660e
Resul <garnnet_221@hotmail.com>
2021-06-04 23:13:14 +0300

    Pawn Tank class added
    Inputs bound for movement and rotation.

---
+++ Source/ToonTanks/Scripts/Pawns/PawnTank.h
@@ -0,0 +1,53 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PawnBase.h"
+#include "PawnTank.generated.h"
+
+class USpringArmComponent;
+class UCameraComponent;
+
+UCLASS()
+class TOONTANKS_API APawnTank : public APawnBase
+{
+	GENERATED_BODY()
+
+	public:
+	
+	// Sets default values for this pawn's properties
+	APawnTank();
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	
+	private:
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Components", meta = (AllowPrivateAccess = "true"))
+	USpringArmComponent* SpringArm = nullptr;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Components", meta = (AllowPrivateAccess = "true"))
+	UCameraComponent* Camera = nullptr;
+
+	UPROPERTY(EditAnywhere)
+	float MoveSpeed = 100.f;
+	UPROPERTY(EditAnywhere)
+	float RotationSpeed = 50.f;
+	
+	FVector MoveDirection;
+	FQuat RotationDirection;
+
+	void CalculateMoveInput(float Value);
+	void CalculateRotateInput(float Value);
+
+	void Move();
+	void Rotate();
+
+	
+};
