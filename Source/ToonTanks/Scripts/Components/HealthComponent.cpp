1737772de075911a12e8f2ea928bf26d787c30aa
Resul <garnnet_221@hotmail.com>
2021-06-18 00:48:42 +0300

    HealthComponent Debug completed.

--- Source/ToonTanks/Scripts/Components/HealthComponent.cpp
+++ Source/ToonTanks/Scripts/Components/HealthComponent.cpp
@@ -12,8 +12,6 @@ UHealthComponent::UHealthComponent()
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = false;
-
-	// ...
 }
 
 
@@ -36,6 +34,8 @@ void UHealthComponent::TakeDamage(AActor* DamagedActor, float Damage, const UDam
 		return;
 
 	Health = FMath::Clamp(Health - Damage, 0.0f, DefaultHealth);
+
+	UE_LOG(LogTemp, Warning, TEXT("%s Health is %f"), *GetOwner()->GetName(), Health);
 
 	if(Health <= 0)
 	{
